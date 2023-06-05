50f7ae450040ca6f0c7d20a976fc03ffdc7727bb
Builderhummel <max.kretschmann@tuhh.de>
2023-06-05 14:28:59 +0200

    update func name update...

--- src/backend/item/item_handler.c
+++ src/backend/item/item_handler.c
@@ -1,6 +1,6 @@
 #include "item_object.h"
 
-int update_item(ItemObject_t* item, int id, char* name, int price, int in_warehouse) {
+int item_update(ItemObject_t* item, int id, char* name, int price, int in_warehouse) {
         item->id = id;
         item->name = strdup(name);
         item->price = price;
@@ -9,7 +9,7 @@ int update_item(ItemObject_t* item, int id, char* name, int price, int in_wareho
         return 0;
 }
 
-int update_item_status(ItemObject_t* item, int in_warehouse) {
+int item_update_status(ItemObject_t* item, int in_warehouse) {
         item->in_warehouse = in_warehouse;
 
         return 0;
--- src/backend/item/item_handler.h
+++ src/backend/item/item_handler.h
@@ -3,7 +3,7 @@
 
 #include "item_object.h"
 
-int update_item(ItemObject_t* item, int id, char* name, int price, int in_warehouse);
-int update_item_status(ItemObject_t* item, int in_warehouse);
+int item_update(ItemObject_t* item, int id, char* name, int price, int in_warehouse);
+int item_update_status(ItemObject_t* item, int in_warehouse);
 
 #endif // ITEM_HANDLER_H
